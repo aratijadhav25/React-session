4c68f70284049a666ce1099facc4f87860bc1800
Arati <aratijadhav2503@gmail.com>
2026-08-12 22:31:08 +0530

    hook added

---
+++ react-notes/src/Phase-1-Foundations/Module-03-Fundamentals/04-events/index.js
@@ -0,0 +1,4 @@
+export { default as OnClickBasics } from "./01-onClick-basics";
+export { default as EventObject } from "./02-event-object";
+export { default as onChangeInputs } from "./03-onchange-inputs";
+export { default as PassingHandlers } from "./04-passing-handler";
